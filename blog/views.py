456cb6bca53aebaff8d2a00721e51c10d9c8858f
tareqobaida <tareqbuet@gmail.com>
2016-09-29 19:32:31 -0500

    Blog fine tune

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post, BlogCategory, BlogTag
 
 
 def blog(request, author_id=None, cat_id=None, tag_id=None):
-    blogs = Post.objects.filter(post_type='blog').order_by('date_created')
+    blogs = Post.objects.filter(post_type='blog')
     if author_id is not None:
         blogs = blogs.filter(author_id=author_id)
     elif cat_id is not None:
@@ -12,7 +12,7 @@ def blog(request, author_id=None, cat_id=None, tag_id=None):
         blogs = blogs.filter(tag=tag_id)
 
     context = {
-        'blogs': blogs[:20],
+        'blogs': blogs.order_by('-date_created')[:20],
     }
     cat_tag_context = get_cats_tags()
     context = {**context, **cat_tag_context}  # new in python3.5. add two dict
@@ -21,7 +21,7 @@ def blog(request, author_id=None, cat_id=None, tag_id=None):
 
 def blog_single(request, post_id):
     blog = Post.objects.get(id=post_id)
-    recent_blogs = Post.objects.filter(post_type='blog').order_by('date_created')[:10]
+    recent_blogs = Post.objects.filter(post_type='blog').order_by('-date_created')[:10]
     context = {
         'blog': blog,
         'recent_blogs':recent_blogs,
