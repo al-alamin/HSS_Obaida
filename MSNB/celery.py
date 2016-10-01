7bac640df207c88bf2d0ca2d336a6c42b46cfeb1
alalamin25 <mdabdullahalalaminp@gmail.com>
2016-10-01 12:02:26 +0600

    celery initial settings working

---
+++ MSNB/celery.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.local')
+
+from django.conf import settings  # noqa
+
+app = Celery('MSNB',
+             BROKER_URL = 'amqp://guest:guest@localhost:5672//',
+             # BROKER_URL = 'django://',
+             backend='amqp://',
+             # include=['common.tasks']
+             )
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.conf.update(
+    CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
+)
+app.conf.update(
+    CELERY_RESULT_BACKEND='djcelery.backends.cache:CacheBackend',
+)
+
+app.conf.update(
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
+    CELERY_RESULT_SERIALIZER='json',
+    CELERY_TIMEZONE='Europe/Oslo',
+    CELERY_ENABLE_UTC=True,
+)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+if __name__ == '__main__':
+    app.start()
