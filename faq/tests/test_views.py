b9c85b47c15273030a92644d4b94b771f0fb8c68
alalamin25 <mdabdullahalalaminp@gmail.com>
2016-10-02 23:47:28 +0600

    wrote some tests on faq

--- faq/tests/test_views.py
+++ faq/tests/test_views.py
@@ -1,51 +1,10 @@
 from django.test import TestCase
 
-from common import models
+from faq.models import Question
+from common.models import Tag, Category, Type
+from faq.forms import FaqSearchForm
 
-
-class TestRelatedModels(TestCase):
-    def setUp(self):
-        self.insert_data_into_models()
-
-    def insert_data_into_models(self):
-        type = models.Type(name='Standard Test')
-        type.save()
-        cat = models.Category(name='GRE', type=type)
-        cat.save()
-        cat = models.Category(name='TOEFL', type=type)
-        cat.save()
-        type = models.Type(name='News')
-        type.save()
-        cat = models.Category(name='News', type=type)
-        cat.save()
-        author = models.Author(first_name='abu', last_name='obaida', email='tareqbuet@gmail.com')
-        author.save()
-        question = models.Question(author=author, text='sample question')
-        question.save()
-        ans = models.Answer(author=author, question=question, text='sample ans 1')
-        ans.save()
-        question = models.Question(author=author, text='sample question2')
-        question.save()
-        ans = models.Answer(author=author, question=question, text='sample ans 2-1')
-        ans.save()
-
-
-    def test_categoris_arranged_by_types(self):
-        types = models.Type.objects.all().exclude(name='News')
-        # for type in types:
-        #     print('type', type.name)
-        #     for cat in type.category_set.all():
-        #         print(cat.name)
-
-    def test_can_get_all_questions_and_corresponding_answer_set(self):
-        questions = models.Question.objects.all()
-        self.assertTrue(questions)
-
-    def test_can_get_recent_questions(self):
-        latest = models.Question.objects.all().order_by('date')[:2]
-        for q in latest:
-            print('latest', q.date)
-            print('ans', q.answer.text)
+from django.core.urlresolvers import reverse
 
 
 class FaqTest(TestCase):
@@ -61,7 +20,36 @@ class FaqTest(TestCase):
 
     def test_faq_page_uses_question_model(self):
         response = self.client.get('/faq/')
-        if (models.Question.objects.all().count() > 0):
+        if (Question.objects.all().count() > 0):
             self.assertIsInstance(
                 response.context['recent_q'], models.Question)
         # question = Question.objects.create(text="new question")
+
+
+class TestFaqSearchResult(TestCase):
+
+    def setUp(self):
+        type = Type.objects.create(name="type")
+        self.tag = Tag.objects.create(name="tag", type=type)
+        self.category = Category.objects.create(name="category")
+        question = Question(text="question", ans="ans")
+        question.save()
+        question.category.add(self.category)
+        question.tag.add(self.tag)
+
+    def test_faq_url_status_code(self):
+        response = self.client.get(reverse("faq"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_faq_search_cat_url_status(self):
+        response = self.client.get(reverse("faq_search_cat", args=[self.category.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_faq_search_tag_url_status(self):
+        response = self.client.get(reverse("faq_search_cat", args=[self.tag.id]))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_faq_search_url_status(self):
+        response = self.client.post(reverse("faq_search"), {'search_item': 'a'})
+        self.assertEqual(response.status_code, 200)
+
