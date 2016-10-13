47f6fa2a06c6dea6bd54264c3e8eab43fa3ca1c5
tareqobaida <tareqbuet@gmail.com>
2016-10-12 21:20:41 -0500

    separated different part of template of download center app in different files

--- download_center/urls.py
+++ download_center/urls.py
@@ -3,5 +3,5 @@ from .views import download
 
 urlpatterns = [
     url(r'^$', download, name='download'),
-    url(r'^type/(?P<department_id>\d+)/$', download, name='download_search_department')
+    url(r'^department/(?P<department_id>\d+)/$', download, name='download_search_department')
 ]
--- download_center/views.py
+++ download_center/views.py
@@ -3,10 +3,13 @@ from .models import Type, Department, Document
 
 
 def download(request, department_id=None):
-    types = Type.objects.all().order_by('name')
+    types = Type.objects.all().order_by('name').prefetch_related()
+    if department_id is not None:
+        types = types.filter(document__department__id = department_id)
     departments = Department.objects.all().order_by('name')
     context = {
         'types': types,
-        'departments': departments
+        'departments': departments,
+        'department_id':department_id
     }
     return render(request, 'download_center/download.html', context)
