51fe0a694062791f3b4775490421bf376b418e2b
alalamin25 <mdabdullahalalaminp@gmail.com>
2016-10-14 16:15:46 +0600

    adding tests to contact_us app

---
+++ contact_us/tests/test_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from contact_us.forms import ContactUsForm
+
+from django.core.urlresolvers import reverse
+
+
+class ContacTUsTest(TestCase):
+
+    def test_contact_us_view_renders_contact_us_template(self):
+        response = self.client.get(reverse("contactus"))
+        self.assertTemplateUsed(response, 'contact_us/contact_us.html')
+
+    def test_contact_us_page_uses_contact_us_form(self):
+        response = self.client.get(reverse("contactus"))
+        self.assertIsInstance(
+            response.context['form'], ContactUsForm)
