b51ee413b5da023a493f37636c0b939c4240d4f0
tareqobaida <tareqbuet@gmail.com>
2016-10-14 15:08:39 -0500

    FB like, share, comment integrated with faq

--- download_center/models.py
+++ download_center/models.py
@@ -2,24 +2,29 @@ from django.db import models
 
 
 class Type(models.Model):
-    name = models.CharField(max_length=25, help_text='e,g; SOP, Resume, Mail, Others')
+    name = models.CharField(max_length=25, unique=True,help_text='e,g; SOP, Resume, Mail, Others')
 
     def __str__(self):
         return self.name
 
 
 class Department(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Document(models.Model):
+    degree_choices = (
+        ('Undergrad','Undergrad'),
+        ('Masters', 'Masters'),
+        ('PhD', 'PhD'),
+    )
     type = models.ForeignKey(Type)
     department = models.ForeignKey(Department)
-    degree = models.CharField(choices=(('MS', 'MS'), ('PhD', 'PhD')), max_length=20)
-    comment = models.CharField(max_length=500)
+    degree = models.CharField(choices=degree_choices, max_length=20)
+    comment = models.CharField(max_length=500, blank=True)
     download_link = models.URLField()
 
     def __str__(self):
