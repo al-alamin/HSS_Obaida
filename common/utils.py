a492dc9ef81ea17739ee2219460cb83a1570e140
al alamin <mdabdullahalalaminp@gmail.com>
2016-10-19 14:37:16 +0600

    Fixing issue with html email

--- common/utils.py
+++ common/utils.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 ADMIN_EMAILS = settings.ADMIN_EMAILS
 PRIMARY_ADMIN_EMAIL = settings.PRIMARY_ADMIN_EMAIL
 
+
 @app.task
 def send_mail(subject, body, to_email=ADMIN_EMAILS,
               from_email=PRIMARY_ADMIN_EMAIL, bcc=None, attachment=None):
@@ -16,6 +17,8 @@ def send_mail(subject, body, to_email=ADMIN_EMAILS,
         "In util.send_mail method sending mail to {0}".format(to_email))
     email_success = False
     email = EmailMessage(subject, body, from_email, to_email, bcc)
+    # This content_subtype is added to properly render html in email_body
+    email.content_subtype = "html"
     if attachment is not None:
         email.attach(
             attachment.name, attachment.read(), attachment.content_type)
