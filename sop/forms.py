34401ee5598b57464276b15624ce0d6301828847
tareqobaida <tareqbuet@gmail.com>
2016-10-25 22:48:12 -0500

    Fixed sending sop via mail

--- sop/forms.py
+++ sop/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from common.utils import send_mail
 
 SUPPORTED_SOP_FILE_TYPES = ['application/msword',
@@ -28,5 +29,5 @@ class SOPSubmitForm(forms.Form):
         from_email = self.cleaned_data['email']
         msg = self.cleaned_data['msg']
         subject = "{0} submitted a SOP for review".format(name)
-        email_success = send_mail(subject, msg, from_email=from_email, attachmets=file)
+        email_success = send_mail(subject, msg, from_email=from_email, attachment=file)
         return email_success
