d50d4f629aabd76ab02436baae4280128e417ac1
tareqobaida <tareqbuet@gmail.com>
2016-10-25 23:10:39 -0500

    sop reviewer section deleted

--- sop/views.py
+++ sop/views.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.models import User
-from random import shuffle
-
 from django.shortcuts import render
 
 from .forms import SOPSubmitForm
@@ -15,12 +12,8 @@ def sop(request):
             email_success = form.email_SOP(file)
     else:
         form = SOPSubmitForm()
-    reviewers = User.objects.filter(groups__name='Reviewer').distinct()
-    # show reviewers in random
-    reviewers = list(reviewers)  # for shuffling purpose
-    shuffle(reviewers)
 
     context = {'form': form,
                'email_success': email_success,
-               'reviewers': reviewers}
+               }
     return render(request, 'sop/sop_review.html', context)
