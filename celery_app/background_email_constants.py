fcc5d7ddaaab2e6d2e11f1b55bcc67fb00e1b3ef
tareqobaida <tareqbuet@gmail.com>
2016-11-14 09:58:00 -0600

    Email template changed to match html. <br> tag is inserted where necessary.

--- celery_app/background_email_constants.py
+++ celery_app/background_email_constants.py
@@ -10,62 +10,51 @@ FEEDBACK_REMINDER_MINUTE = 30
 SUBJECT_REMINDER = "Your Reminder Email For The Event {0}"
 SUBJECT_FEEDBACK = "We Appreciate Your Feedback"
 
-BODY_REMINDER = """
-     Hi {0},
-     Hope you are doing well. This is a reminder email for the event {1} which you are registered for.
-     The event will be held on {2} Bangladesh time and the event duration is {3} minutes.
-     If you have any query please don't hesitate to email at support@mystudynotebook.com.
-     Please don't forget to receive call from our Skype account (Skype ID is 'MSNB') at the mentioned time.
-     We hope a very good day to you.
-
-     Thanks and Regards,
-     Support Team
-     My Study Notebook
-     Web: http://www.mystudynotebook.com
-     Facebook Group: https://www.facebook.com/groups/mystudynotebook/
+SIGNATURE = """
+     Thanks and Regards,<br>
+     Support Team <br>
+     My Study Notebook <br>
+     Web: http://www.mystudynotebook.com <br>
+     Facebook Group: https://www.facebook.com/groups/mystudynotebook/ <br>
      """
 
-BODY_FEEDBACK = """
-    Hi {0},
-    Thank you for participating in the event {1}.We would appreciate if you could give us some feedback on how was the skype session, if it met your expectations, how can we improve it etc. Let us know if you feel the session was helpful enough and if you would recommend it to your friends.
-    Your feedback would help us improve the endeavor for helping other students in their venture.
-    Please feel free to write to us at support@mystudynotebook.com or our Facebook group https://www.facebook.com/groups/mystudynotebook/.
-    Again thanks for joining the session.
+BODY_REMINDER = """
+     Hi {0}, <br>
+     Hope you are doing well. This is a reminder email for the event {1} which you are registered for.
+     The event will be held on {2} Bangladesh time and the event duration is {3} minutes.
+     If you have any query please don't hesitate to email at support@mystudynotebook.com.<br>
+     Please don't forget to receive call from our Skype account (Skype ID is 'MSNB') at the mentioned time.<br>
+     We hope a very good day to you.<br> <br>
+     """ + SIGNATURE
 
-    Thanks and Regards,
-    Support Team
-    My Study Notebook
-    Web: http://www.mystudynotebook.com
-    Facebook Group: https://www.facebook.com/groups/mystudynotebook/
-    """
+BODY_FEEDBACK = """
+    Hi {0}, <br>
+    Thank you for participating in the event {1}.We would appreciate if you could give us some feedback on how was
+    the skype session, if it met your expectations, how can we improve it etc. Let us know if you feel the session was
+    helpful enough and if you would recommend it to your friends.Your feedback would help us improve the endeavor for
+    helping other students in their venture.<br>
+    Please feel free to write to us at support@mystudynotebook.com or our Facebook group
+    https://www.facebook.com/groups/mystudynotebook/. <br>
+    Again thanks for joining the session. <br><br>
+    """ + SIGNATURE
+
 body_event_registration = """
-    Hi {0},
-    Your registration is confirmed for the event {1}.
-    The event will be held on {2} Bangladesh time and the event duration is {3} minutes.
-    If you have any query please don't hesitate to email at support@mystudynotebook.com.
+    Hi {0}, <br>
+    Your registration is confirmed for the event {1}.<br>
+    The event will be held on {2} Bangladesh time and the event duration is {3} minutes.<br>
+    If you have any query please don't hesitate to email at support@mystudynotebook.com.<br>
     Please don't forget to receive call from our Skype account (Skype ID is 'MSNB') at the mentioned time.
     We hope a very good day to you.<br>
     {4}
+   <br><br>
+    """ + SIGNATURE
 
-    <br>
-    Thanks and Regards,<br>
-    Support Team <br>
-    My Study Notebook <br>
-    Web: http://www.mystudynotebook.com <br>
-    Facebook Group: https://www.facebook.com/groups/mystudynotebook/ <br>
-    """
 body_new_user = """
-    Hi,
-    Congratulations and welcome to My Study Notebook. You have successfully created your account and now you have access to all the features of My Study Notebook. Below is your account information:
-    Your Username : {0}
-    Your Registered Email : {1}
-    Your Authentication Provider: {2}
-
-    Thanks again for joining. We hope a very good day to you.
-
-    Thanks and Regards,
-    Support Team
-    My Study Notebook
-    Web: http://www.mystudynotebook.com
-    Facebook Group: https://www.facebook.com/groups/mystudynotebook/
-    """
+    Hi,<br>
+    Congratulations and welcome to My Study Notebook. You have successfully created your account and now you have access
+    to all the features of My Study Notebook. Below is your account information:<br>
+    Your Username : {0} <br>
+    Your Registered Email : {1} <br>
+    Your Authentication Provider: {2} <br>
+    Thanks again for joining. We hope a very good day to you.<br><br>
+    """ + SIGNATURE
