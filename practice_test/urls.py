6e37c141988e11b9cb5a91b011718d71664809e3
al alamin <mdabdullahalalaminp@gmail.com>
2016-12-14 15:25:46 +0600

    gre subject test rules

--- practice_test/urls.py
+++ practice_test/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from practice_test.views import GreView
+from practice_test.views import GreView, GreSubjectTestRulesView
 from django.views.generic import TemplateView
-from django.views import View
+
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(
         template_name="practice_test/practice_test.html"),
         name='practice_test'),
     url(r'^gre/$', GreView.as_view(), name='gre'),
+    url(r'^gre/subject_test/(?P<id>[0-9]+)/rules$',
+        GreSubjectTestRulesView.as_view(), name='gre_subject_test_rules'),
 ]
--- practice_test/views.py
+++ practice_test/views.py
@@ -10,7 +10,23 @@ class GreView(View):
     def get(self, request, *args, **kwargs):
         subject_test = SubjectTest.objects.filter(exam_type='gre')
         context = {
-        'subject_test': subject_test,
+            'subject_test': subject_test,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+
+class GreSubjectTestRulesView(View):
+
+    template_name = 'practice_test/gre_subject_test_rules.html'
+
+    def get(self, request, *args, **kwargs):
+        subject_test_id = self.kwargs['id']
+        subject_test = SubjectTest.objects.get(id=subject_test_id)
+        context = {
+            'subject_test': subject_test,
         }
         return render(request, self.template_name, context)
 
